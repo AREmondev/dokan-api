0d7a3e67d58c0e48cc3ca324d46b99b6860420ec
AREmondev <abdurrahmanemondev@gmail.com>
2022-09-07 17:41:44 +0600

    db change

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = () => {
   try {
     mongoose.connect(
-      'mongodb+srv://emon:emon1122@cluster0.luzy6.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://emon:emon1122@cluster0.luzy6.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://emon:emon1122@cluster0.luzy6.mongodb.net/admin?authSource=admin&replicaSet=atlas-xnf59p-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
       { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
       console.log('Connected'),
     )
